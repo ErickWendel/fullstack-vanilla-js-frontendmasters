e7ee909b5fe4fca6a81a8ec87bcfeb02fb428e08
Erick Wendel <erick.workspace@gmail.com>
2024-08-28 22:44:35 -0500

    update script
    Signed-off-by: Erick Wendel <erick.workspace@gmail.com>

--- app/web-api/src/index.js
+++ app/web-api/src/index.js
@@ -6,8 +6,11 @@ const PORT = process.env.PORT || 3000
 const server = http.createServer(handler)
 
 if (process.env.NODE_ENV !== 'test')
-  server.listen(PORT, () => console.log(`server is running at  ${PORT}`))
+  server.listen(PORT, () =>
+    console.log(`server is running at  ${PORT}`)
+  )
 
 export {
   server
 }
+
